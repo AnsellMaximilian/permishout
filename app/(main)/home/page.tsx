f567059f75acf80e187b8a5e8e015342aa22974e
Ansell Maximilian <ansellmaximilian@gmail.com>
2025-05-03 06:11:25 +0700

    loader for shouts

--- app/(main)/home/page.tsx
+++ app/(main)/home/page.tsx
@@ -3,16 +3,25 @@
 import ShoutComposer from "@/components/shouts/ShoutComposer";
 import ShoutItem from "@/components/shouts/ShoutItem";
 import api from "@/lib/api";
+import { toastError } from "@/lib/utils";
 import { Shout } from "@/types/shout";
+import { Loader2 } from "lucide-react";
 import { useEffect, useState } from "react";
 
 export default function HomePage() {
   const [shouts, setShouts] = useState<Shout[]>([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     (async () => {
-      const shouts = await api.get("/shouts");
+      try {
+        const shouts = await api.get("/shouts");
 
-      setShouts(shouts.data as Shout[]);
+        setShouts(shouts.data as Shout[]);
+      } catch {
+        toastError("Error fetching shouts.");
+      } finally {
+        setLoading(false);
+      }
     })();
   }, []);
   return (
@@ -22,9 +31,19 @@ export default function HomePage() {
       </div>
 
       <div className="flex flex-col gap-4">
-        {shouts.map((shout) => (
-          <ShoutItem key={shout.key} shout={shout} />
-        ))}
+        {loading ? (
+          <div className="p-12 flex justify-center items-center">
+            <Loader2 className="animate-spin" size={40} />
+          </div>
+        ) : shouts.length === 0 ? (
+          <div className="p-12">
+            <p className="text-center text-muted-foreground text-sm">
+              No shouts yet. Be the first to shout!
+            </p>
+          </div>
+        ) : (
+          shouts.map((shout) => <ShoutItem key={shout.key} shout={shout} />)
+        )}
       </div>
     </div>
   );
